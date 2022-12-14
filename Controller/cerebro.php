ab4cca80ec63fefc433dcb99283c0fae1aad24e3
Gerardo <gerardomiranda2498@gmail.com>
2022-12-14 11:31:49 +0100

    MVC animales incompleto

--- Controller/cerebro.php
+++ Controller/cerebro.php
@@ -1,33 +1,42 @@
 <?php
     require "../Model/Animal.php";
     require "../Model/Adopcion.php";
+    //require "../View/vistaAnimal.php";
 
     $tablaSeleccionada = $_POST['opcion'];
 
-
-
-    if ($tablaSeleccionada == 'animales') {
-        function datosTablaAnimal()
-        {
-            $usuario = new Animal('pepe ', '', '', '', '', '', 'protectora_animales');
-            $usuario->datosTablaAnimal();
-        }
-        datosTablaAnimal();
-    } else if ($tablaSeleccionada == 'adopcion') {
-        function datosTablaAdp()
-        {
-            $usuario = new Adopcion('', '', '', '', 'protectora_animales');
-            $usuario->datosTablaAdopcion();
-            
-        }
-        datosTablaAdp();
-
+    if(isset($_POST['mostrarBoton'])){
+        $animal = new Animal('pepe ', '', '', '', '', '', 'protectora_animales');
+        $x = new Controlador_Animal($animal);
+        $x->animalesTabla();
+        // if ($tablaSeleccionada == 'animales') {
+        //     function datosTablaAnimal()
+        //     {
+        //         $usuario = new Animal('pepe ', '', '', '', '', '', 'protectora_animales');
+        //         $usuario->datosTablaAnimal();
+        //     }
+        //     datosTablaAnimal();
+        // } else if ($tablaSeleccionada == 'adopcion') {
+        //     function datosTablaAdp()
+        //     {
+        //         $usuario = new Adopcion('', '', '', '', 'protectora_animales');
+        //         $usuario->datosTablaAdopcion();
+                
+        //     }
+        //     datosTablaAdp();
+    
+        // } else {
+        //     function datosTablaUser()
+        //     {
+        //         $usuario = new Usuario('','','','','','','protectora_animales');
+        //         $usuario->datosTablaUsuario();
+        //     }
+        //     datosTablaUser();
+        // }
     } else {
-        function datosTablaUser()
-        {
-            $usuario = new Usuario('','','','','','','protectora_animales');
-            $usuario->datosTablaUsuario();
-        }
-        datosTablaUser();
+        header('location: ../View/index.html');
+        die();
     }
+
+
 ?> 
---
+++ View/vistaAnimal.php
@@ -0,0 +1,19 @@
+<?php
+    require_once "../Controller/Controlador_Animal.php";
+    // $animal = new Animal('pepe ', '', '', '', '', '', 'protectora_animales');
+    // $objControlador = new Controlador_Animal($animal);
+    // $obj = $objControlador->mostrar_Animales();
+    // print_r($obj[0]);
+    // for ($i=0; $i < count($obj); $i++) { 
+    //     # code...
+    //     print_r($obj[$i]);
+    //     echo "<br/>";
+    //     echo "<br/>";
+    // }
+
+    // print_r($obj[0]->nombre);
+
+    
+
+    
+?>
